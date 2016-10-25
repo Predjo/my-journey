4245c7e562bf40445ad519d13c6c40b4cb69d754
Nikola <predovan.nikola@gmail.com>
2016-10-25 02:38:38 +0200

    remove console logs

--- server/modules/utils/functions/match-route.js
+++ server/modules/utils/functions/match-route.js
@@ -5,21 +5,27 @@ const match = require('react-router').match;
 
 module.exports = function matchRoute(routes, matchCB, noMatchCB) {
   return function  (req, res) {
-    console.log(req.url);
-    console.log(req.originalUrl);
     match({ routes, location : req.originalUrl }, (error, redirectLocation, renderProps) => {
       if (error) {
+
         res.status(500).send(error.message);
+
       } else if (redirectLocation) {
+
         res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+
       } else if (renderProps) {
+
         matchCB(req, res, renderProps);
+
       } else {
+
         if (noMatchCB) {
           noMatchCB(req, res);
         } else {
           res.status(404).send('Not found');
         }
+
       }
     });
   };
